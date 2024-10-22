c2824a0c0771d0198c6f5a14939b4c80ab2b5ae4
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-23 00:10:12 +0900

    custom-preference: Update OverlaySwitchPreference from YAAP
    OverlaySwitchPreference: Allow disabling an overlay in addition
    
    Will always be toggled to the not state of the main overlay in key
    Also improve the switch and allow setting an RRO of package:overlay format
    
    OverlaySwitchPreference: Allow config dkey on night mode only
    
    OverlaySwitchPreference: Fixup wrong defStyleAttr
    
    Added in the move to Compat switch and forgotten behind

--- src/com/android/settings/preferences/OverlaySwitchPreference.java
+++ src/com/android/settings/preferences/OverlaySwitchPreference.java
@@ -16,69 +16,111 @@
 
 package com.android.settings.preferences;
 
+import static android.os.UserHandle.CURRENT;
 import static android.os.UserHandle.USER_CURRENT;
 
 import android.content.Context;
-import android.content.om.IOverlayManager;
+import android.content.om.OverlayManager;
+import android.content.om.OverlayManagerTransaction;
+import android.content.om.OverlayIdentifier;
 import android.content.om.OverlayInfo;
+import android.content.res.Configuration;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 import android.util.Log;
 
-import androidx.preference.SwitchPreferenceCompat;
-
 import java.lang.SecurityException;
+import java.util.List;
 
-public class OverlaySwitchPreference extends SwitchPreferenceCompat {
+import com.android.settings.preferences.SelfRemovingSwitchPreference;
+
+public class OverlaySwitchPreference extends SelfRemovingSwitchPreference {
+
     private final static String TAG = "OverlaySwitchPreference";
-    private IOverlayManager mOverlayManager;
+    private final static String SETTINGSNS = "http://schemas.android.com/apk/res-auto";
+    private static final String DKEY = "dkey";
+    private static final String DKEY_NIGHT_ONLY = "dkeyNightOnly";
 
-    public OverlaySwitchPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-    }
+    private final String mDisableKey;
+    private final boolean mDKeyNightOnly;
+    private final OverlayManager mOverlayManager;
 
-    public OverlaySwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    public OverlaySwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mDisableKey = attrs.getAttributeValue(SETTINGSNS, DKEY);
+        mDKeyNightOnly = attrs.getAttributeBooleanValue(SETTINGSNS, DKEY_NIGHT_ONLY, false);
+        mOverlayManager = context.getSystemService(OverlayManager.class);
     }
 
     public OverlaySwitchPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mDisableKey = attrs.getAttributeValue(SETTINGSNS, DKEY);
+        mDKeyNightOnly = attrs.getAttributeBooleanValue(SETTINGSNS, DKEY_NIGHT_ONLY, false);
+        mOverlayManager = context.getSystemService(OverlayManager.class);
     }
 
     public OverlaySwitchPreference(Context context) {
-        super(context);
+        this(context, null);
     }
 
     @Override
-    public void onAttached() {
-        super.onAttached();
-        mOverlayManager = IOverlayManager.Stub.asInterface(
-                ServiceManager.getService(Context.OVERLAY_SERVICE));
-        if (mOverlayManager == null) return;
+    protected boolean isPersisted() {
+        return true;
+    }
+
+    @Override
+    protected boolean getBoolean(String key, boolean defaultValue) {
+        if (mOverlayManager == null) return false;
         OverlayInfo info = null;
-        try {
-            info = mOverlayManager.getOverlayInfo(getKey(), USER_CURRENT);
-        } catch (RemoteException e) {
-            Log.e(TAG, "Failed getting overlay " + getKey() + " info");
-            e.printStackTrace();
-        }
-        if (info != null) {
-            setChecked(info.isEnabled());
-        }
+        info = mOverlayManager.getOverlayInfo(getOverlayID(getKey()), CURRENT);
+        if (info != null) return info.isEnabled();
+        return false;
     }
 
     @Override
-    public void setChecked(boolean checked) {
+    protected void putBoolean(String key, boolean value) {
         if (mOverlayManager == null) return;
+        OverlayManagerTransaction.Builder transaction = new OverlayManagerTransaction.Builder();
+        transaction.setEnabled(getOverlayID(getKey()), value, USER_CURRENT);
+        if (mDisableKey != null && !mDisableKey.isEmpty()) {
+            if (mDKeyNightOnly) {
+                final boolean isNight = (getContext().getResources().getConfiguration().uiMode
+                    & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES;
+                if (isNight)
+                    transaction.setEnabled(getOverlayID(mDisableKey), !value, USER_CURRENT);
+                else // always enabled in day
+                    transaction.setEnabled(getOverlayID(mDisableKey), true, USER_CURRENT);
+            } else {
+                transaction.setEnabled(getOverlayID(mDisableKey), !value, USER_CURRENT);
+            }
+        }
         try {
-            mOverlayManager.setEnabled(getKey(), checked, USER_CURRENT);
-        } catch (SecurityException | RemoteException e) {
-            Log.e(TAG, "Failed setting overlay " + getKey() + " to " +
-                    (checked ? "enabled" : "disabled"));
+            mOverlayManager.commit(transaction.build());
+        } catch (SecurityException | IllegalStateException e) {
+            Log.e(TAG, "Failed setting overlay(s), future logs will point the reason");
             e.printStackTrace();
             return;
         }
-        super.setChecked(checked);
+    }
+
+    private OverlayIdentifier getOverlayID(String name) throws IllegalStateException {
+        if (mOverlayManager == null) return null;
+        if (name.contains(":")) {
+            // specific overlay name in a package
+            final String[] value = name.split(":");
+            final String pkgName = value[0];
+            final String overlayName = value[1];
+            final List<OverlayInfo> infos =
+                    mOverlayManager.getOverlayInfosForTarget(pkgName, CURRENT);
+            for (OverlayInfo info : infos) {
+                if (overlayName.equals(info.getOverlayName()))
+                    return info.getOverlayIdentifier();
+            }
+            throw new IllegalStateException("No overlay found for " + name);
+        }
+        // package with only one overlay
+        return mOverlayManager.getOverlayInfo(name, CURRENT).getOverlayIdentifier();
     }
 }
